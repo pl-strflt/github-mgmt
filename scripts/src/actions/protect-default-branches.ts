45ea8260678b9ff1cc253a89ad4ba80e972bfc0e
galargh <piotr.galar@gmail.com>
2022-07-24 12:31:00 +0200

    fix: protect default branches action

--- scripts/src/actions/protect-default-branches.ts
+++ scripts/src/actions/protect-default-branches.ts
@@ -14,15 +14,23 @@ async function run(): Promise<void> {
   const repositories = config.getResources([terraform.GithubRepository])
 
   for (const repository of repositories) {
+    // TODO: we need a better abstraction for things that are defined in the YAML
+    //       ideally, we should be able to replace all of this with something like:
+    //       const name = repository.getName()
+    //       const pattern = repository.getPattern()
+    const name = ((repository.value as YAML.Pair).key as YAML.Scalar<string>).value
+    const pattern = (((repository.value as YAML.Pair).value as YAML.YAMLMap).items.find(pair => {
+      return (pair.key as YAML.Scalar<string>).value === 'pattern'
+    })?.value as YAML.Scalar<string>)?.value || 'main'
+
     // TODO: we need a better abstraction here, one which has a proper constructor
     //       and doesn't know anything about terraform (i.e. values.id)
     const rule = new terraform.GithubBranchProtection()
+    rule.index = `${name}:${pattern}`
     rule.values = {
       id: '',
-      repository: ((repository.value as YAML.Pair).key as YAML.Scalar<string>).value,
-      pattern: (((repository.value as YAML.Pair).value as YAML.YAMLMap).items.find(pair => {
-        return (pair.key as YAML.Scalar<string>).value === 'pattern'
-      })?.value as YAML.Scalar<string>)?.value || 'main',
+      repository: name,
+      pattern: pattern,
       required_status_checks: {},
       required_pull_request_reviews: {},
     }
