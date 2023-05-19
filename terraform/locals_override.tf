a53265e78cfe038178af063f4b6b3b50ed03b370
Piotr Galar <piotr.galar@gmail.com>
2023-05-19 08:41:11 +0200

    Update locals_override.tf

--- terraform/locals_override.tf
+++ terraform/locals_override.tf
@@ -1,3 +1,3 @@
 locals {
-  resource_types = ["github_membership", "github_repository_collaborator", "github_repository", "github_team_membership", "github_team_repository", "github_team", "github_repository_file", "github_branch_protection", "github_issue_label"]
+  resource_types = ["github_membership", "github_repository_collaborator", "github_repository", "github_team_membership", "github_team_repository", "github_team", "github_repository_file", "github_branch_protection"]
 }
