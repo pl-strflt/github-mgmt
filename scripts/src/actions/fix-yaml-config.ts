b1a806f8479ddec2475240a6a69f09b461ad47f9
galargh <piotr.galar@gmail.com>
2023-05-26 13:43:35 +0200

    feat: make sure secret scanning is enabled in all repos

--- scripts/src/actions/fix-yaml-config.ts
+++ scripts/src/actions/fix-yaml-config.ts
@@ -1,8 +1,32 @@
 import 'reflect-metadata'
-import { addFileToAllRepos } from './shared/add-file-to-all-repos'
+import {Repository} from '../resources/repository'
+import {addFileToAllRepos} from './shared/add-file-to-all-repos'
 import {format} from './shared/format'
 import {protectDefaultBranches} from './shared/protect-default-branches'
+import {setPropertyInAllRepos} from './shared/set-property-in-all-repos'
+
+function isInitialised(repository: Repository) {
+  return !['ipdx', 'github-action-releaser'].includes(repository.name)
+}
+
+function isPublic(repository: Repository) {
+  return repository.visibility === 'public'
+}
 
 protectDefaultBranches()
-addFileToAllRepos('.github/dependabot.yml', '.github/dependabot.yml', r => !['ipdx', 'github-action-releaser'].includes(r.name))
+addFileToAllRepos(
+  '.github/dependabot.yml',
+  '.github/dependabot.yml',
+  isInitialised
+)
+setPropertyInAllRepos(
+  'secret_scanning',
+  true,
+  r => isInitialised(r) && isPublic(r)
+)
+setPropertyInAllRepos(
+  'secret_scanning_push_protection',
+  true,
+  r => isInitialised(r) && isPublic(r)
+)
 format()
